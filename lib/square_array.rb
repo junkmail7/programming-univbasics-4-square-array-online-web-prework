f4d70bd4ebe072486c811acc14cf4b599eacf74f
William Pannell <lpann5@gmail.com>
2019-10-02 12:59:57 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,11 @@ def square_array(array)
   array1=[]
   while count!=array.size do
     array1[count]=array[count]**2
-    p array1[count]
     count=count+1
+  end
+  
+  while array1[count] do
+    puts pets[count]
+    counter += 1
   end
 end
